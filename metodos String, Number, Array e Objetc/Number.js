1efdf8d0dfd3759c9b05bb064cb12ecade8c76d2
Luis Soares <luissoaress.dev@gmail.com>
2022-07-08 10:11:55 -0300

    update numbersg

---
+++ metodos String, Number, Array e Objetc/Number.js
@@ -0,0 +1,9 @@
+//Principais  métodos Number
+
+//ParserInt()
+const num = 4.4
+console.log(Number.parseInt(num)) // converte um numero decimal em inteiro 
+
+// ParseFloat()
+const num2 = '3.4'
+console.log(Number.parseFloat(num2)) // converte uma string em number independente de ser um decimal ou não 
